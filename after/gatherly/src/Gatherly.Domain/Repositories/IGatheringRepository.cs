fa03617e72ddd813c4163b8fe3ed157dc1c6e78e
Julian Samuel <julian@juliansamuel.co.uk>
2024-01-21 14:55:33 +0000

    InitialCommit

---
+++ after/gatherly/src/Gatherly.Domain/Repositories/IGatheringRepository.cs
@@ -0,0 +1,17 @@
+﻿using Gatherly.Domain.Entities;
+
+namespace Gatherly.Domain.Repositories;
+
+
+public interface IGatheringRepository
+{
+    Task<Gathering?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+    Task<Gathering?> GetByIdWithCreatorAsync(Guid id, CancellationToken cancellationToken = default);
+
+    Task<Gathering?> GetByIdWithInvitationsAsync(Guid id, CancellationToken cancellationToken = default);
+
+    void Add(Gathering gathering);
+
+    void Remove(Gathering gathering);
+}
---
+++ after/gatherly/src/Gatherly.Persistence/Repositories/GatheringRepository.cs
@@ -0,0 +1,50 @@
+﻿using Gatherly.Domain.Entities;
+using Gatherly.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gatherly.Persistence.Repositories;
+
+internal sealed class GatheringRepository : IGatheringRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public GatheringRepository(ApplicationDbContext dbContext) =>
+        _dbContext = dbContext;
+
+    public async Task<Gathering?> GetByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default)
+    {
+        Gathering? gathering = await _dbContext
+            .Set<Gathering>()
+            .AsSingleQuery()
+            .Include(gathering => gathering.Creator)
+            .Include(gathering => gathering.Attendees)
+            .Include(gathering => gathering.Invitations)
+            .FirstOrDefaultAsync(
+                gathering => gathering.Id == id,
+                cancellationToken);
+
+        return gathering;
+    }
+
+    public async Task<Gathering?> GetByIdWithCreatorAsync(
+        Guid id,
+        CancellationToken cancellationToken = default) =>
+        await _dbContext.Set<Gathering>()
+            .Include(g => g.Creator)
+            .FirstOrDefaultAsync(g => g.Id == id, cancellationToken);
+
+    public async Task<Gathering?> GetByIdWithInvitationsAsync(
+        Guid id,
+        CancellationToken cancellationToken = default) =>
+        await _dbContext.Set<Gathering>()
+            .Include(g => g.Invitations)
+            .FirstOrDefaultAsync(g => g.Id == id, cancellationToken);
+
+    public void Add(Gathering gathering) =>
+        _dbContext.Set<Gathering>().Add(gathering);
+
+    public void Remove(Gathering gathering) =>
+        _dbContext.Set<Gathering>().Remove(gathering);
+}
